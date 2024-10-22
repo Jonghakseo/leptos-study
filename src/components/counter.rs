f0aa119776bef08ffbf4d3c27ddd04797828d1f0
Jonghakseo <jonghak.seo@creatrip.com>
2024-10-22 17:00:47 +0900

    feat: add test button

--- src/components/counter.rs
+++ src/components/counter.rs
@@ -1,5 +1,6 @@
 use crate::components::ui::layout::Flex;
 use crate::v;
+use leptos::leptos_dom::logging::console_log;
 use leptos::web_sys::{console, MouseEvent};
 use leptos::*;
 
@@ -9,8 +10,15 @@ pub fn Counter() -> impl IntoView {
 
   let increment = move |e: MouseEvent| {
     set_count.update(|prev| *prev += 1);
+    console_log(&(count.get().to_string()));
     console::log_1(&e);
   };
+
+  console_log(&(count.get().to_string()));
+
+  fn print(_: MouseEvent) {
+    console_log("test");
+  }
 
   view! {
     <Flex vertical=true gap=v!(8)>
@@ -18,6 +26,7 @@ pub fn Counter() -> impl IntoView {
       <h2>{count}</h2>
       <button on:click=increment>"Increment"</button>
       <button on:click=move |_| set_count(count.get() - 1)>"Decrement"</button>
+      <button on:click=print>"test"</button>
     </Flex>
   }
 }
